d0d268c865e7c60bcfcf91d3e5c6faa7377ff8ab
ylug <amaterasumyeye@gmail.com>
2023-09-26 23:01:12 +0300

    "add str_func"

--- str_func.py
+++ str_func.py
@@ -1,5 +1,9 @@
 def str_func(user_str):
+    """принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return user_str.upper()
 
 
+def str_func_two(user_str):
+    """ эта функция делает заглавными первые буквы каждого слова в строке """
+    return user_str.title()
 
